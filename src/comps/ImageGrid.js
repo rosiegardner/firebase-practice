9ecfb7b46fb3dd51e1c2051df4da314d78b4ba2e
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-25 16:13:14 -0700

    update code

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import useFirestore from '../hooks/useFirestore';
 
 const ImageGrid = () => {
-
+  const { docs } = useFirestore('images');
+  console.log(docs);
   return (
     <div className='img-grid'>
-
+      { docs && docs.map(doc => (
+        <div className='img-wrap' key={doc.id}>
+          <img src={doc.url} alt='shrek'></img>
+        </div>
+      ))}
     </div>
   )
 
