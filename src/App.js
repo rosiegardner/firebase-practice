b5a33cb877fc8bc291b584a48db60756102339f1
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-26 12:12:55 -0700

    update Modal

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Title/>
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
-      <Modal selectedImg={selectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/> }
     </div>
   );
 }
