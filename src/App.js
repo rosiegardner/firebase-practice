5155bbfb23313c60b71e8a9f7a78dd19846fa760
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-26 11:53:15 -0700

    update components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title/>
       <UploadForm />
-      <ImageGrid />
-      <Modal />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      <Modal selectedImg={selectedImg}/>
     </div>
   );
 }
