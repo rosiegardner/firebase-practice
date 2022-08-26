ffaa40d23b7a90a2f2a52906dfd9cb9dced301f6
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-26 11:45:40 -0700

    creating the model

---
+++ src/comps/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Modal = () => {
+
+  return (
+    <div className="backdrop">
+      <img src="" alt="enlarge" />
+    </div>
+  )
+  
+}
+
+export default Modal;
